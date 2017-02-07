b90c4417d0e17d76cd3f169fabee5c896f4ad3fd
GRADY <gjenkins205@gmail.com>
2017-02-06 19:26:17 -0500

    Initial Commit

---
+++ AnonChattr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnonChattr
+//
+//  Created by Grady Jenkins on 2/6/17.
+//  Copyright © 2017 GradyJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
